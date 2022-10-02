86c0e5895370eaf646846276868ce2673158ac96
ilis137 <iliyasshahapure1@gmail.com>
2022-10-02 08:19:06 +0530

    add : elcome mesaage

---
+++ src/main/java/com/javapractice/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.javapractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UserRegistration 
+{
+    private static final Logger log = LogManager.getLogger(UserRegistration.class);
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to User Registration Program");
+    }
+}
