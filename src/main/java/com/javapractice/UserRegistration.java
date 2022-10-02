ba5ead6ffd18b5c1675c9f29d501db40577ecd56
ilis137 <iliyasshahapure1@gmail.com>
2022-10-02 09:35:46 +0530

    add:password validation for minimum 8 characters

--- src/main/java/com/javapractice/UserRegistration.java
+++ src/main/java/com/javapractice/UserRegistration.java
@@ -11,24 +11,32 @@ public class UserRegistration
     private static final Logger log = LogManager.getLogger(UserRegistration.class);
     static Scanner sc = new Scanner(System.in);
 
+    //asks users for the details to validate
     public void fillForm(){
         log.info("Enter a valid first name");
 		String firstName = sc.nextLine();
         log.info("Enter a valid first name");
 		String lastName = sc.nextLine();
-        System.out.println("Enter a valid email");
+        log.info("Enter a valid email");
 		String email = sc.nextLine();
-        System.out.println("Enter a valid mobile number starting with country code followed by a space and 10 digit number");
+        log.info("Enter a valid mobile number starting with country code followed by a space and 10 digit number");
 		String mobile = sc.nextLine();
-		validation(firstName,lastName,email,mobile);
+        log.info("Enter a valid password with minimum 8 charcters");
+		String password = sc.nextLine();
+		validation(firstName,lastName,email,mobile,password);
         sc.close();
     }
- 
-    public void validation(String firstName,String lastName,String email,String mobile){
+    //method to validate form data
+    public void validation(String firstName,String lastName,String email,String mobile,String password){
+        // First Name and Last Name comes with restriction such as first letter should be upper case and with minimum 3 characters
         log.info("First Name Valid: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
         log.info("Last Name Valid: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
+        //  abc.xyz@bl.co.in is valid with abc bl and co are mandatory 2 optional(xyz,in) with precise @ and . positions 
         log.info("E-Mail valid: " + Pattern.matches("^[a-zA-Z0-9]{3,}([.]{1}+[a-zA-Z0-9]{3,})*[@]{1}[a-zA-Z0-9]{2,}[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z0-9]{2,})*$", email));
+        // country code followed by space followed by 10 digit number
         log.info("Mobile Number: " + Pattern.matches("^[1-9][0-9]*[\\s][6-9][0-9]{9}$", mobile));
+        // minimum 8 characters required
+		log.info("Password: " + Pattern.matches("^[a-zA-Z0-9]{8,}$", password));
     }
     public static void main( String[] args )
     {
