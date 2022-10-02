ccfd154b12ca53c1a9166ec057d659757dbdf5dd
ilis137 <iliyasshahapure1@gmail.com>
2022-10-02 09:27:50 +0530

    add:mobile number validation

--- src/main/java/com/javapractice/UserRegistration.java
+++ src/main/java/com/javapractice/UserRegistration.java
@@ -10,6 +10,7 @@ public class UserRegistration
 {
     private static final Logger log = LogManager.getLogger(UserRegistration.class);
     static Scanner sc = new Scanner(System.in);
+
     public void fillForm(){
         log.info("Enter a valid first name");
 		String firstName = sc.nextLine();
@@ -17,14 +18,17 @@ public class UserRegistration
 		String lastName = sc.nextLine();
         System.out.println("Enter a valid email");
 		String email = sc.nextLine();
-		validation(firstName,lastName,email);
+        System.out.println("Enter a valid mobile number starting with country code followed by a space and 10 digit number");
+		String mobile = sc.nextLine();
+		validation(firstName,lastName,email,mobile);
         sc.close();
     }
  
-    public void validation(String firstName,String lastName,String email){
+    public void validation(String firstName,String lastName,String email,String mobile){
         log.info("First Name Valid: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
         log.info("Last Name Valid: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
         log.info("E-Mail valid: " + Pattern.matches("^[a-zA-Z0-9]{3,}([.]{1}+[a-zA-Z0-9]{3,})*[@]{1}[a-zA-Z0-9]{2,}[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z0-9]{2,})*$", email));
+        log.info("Mobile Number: " + Pattern.matches("^[1-9][0-9]*[\\s][6-9][0-9]{9}$", mobile));
     }
     public static void main( String[] args )
     {
